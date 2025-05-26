656d45baf415bc1c807bbaf8b834dfcbf7c9031a
AnnyKosova <la62@bk.ru>
2025-05-27 00:00:44 +0300

    подключен конфиг преттира

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,20 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default [
   js.configs.recommended,
+  eslintConfigPrettier,
   prettierRecommended,
   {
-    files: ['js/**/*.js'],
+    files: ['**/*.js'],
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
       sourceType: 'module',
-    },
-    rules: {
-      indent: ['error', 2],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
     },
   },
 ];
