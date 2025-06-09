07abeabd116d4382f096a4b5e008297af856956d
AnnyKosova <la62@bk.ru>
2025-06-09 20:24:34 +0300

    Подключила приложение к API (Получение и добавление новых комментариев через API), создан файл api.js, удален comments.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 import { renderComments } from './render.js';
 import { setupNewCommentHandler } from './eventHandlers.js';
+import { getComments } from './api.js';
 
-renderComments();
-setupNewCommentHandler();
+async function initApp() {
+  try {
+    const comments = await getComments();
+    renderComments(comments);
+    setupNewCommentHandler(comments);
+  } catch (error) {
+    console.error('Ошибка при инициализации приложения:', error);
+    alert('Не удалось загрузить комментарии. Пожалуйста, попробуйте позже.');
+  }
+}
+
+initApp();
